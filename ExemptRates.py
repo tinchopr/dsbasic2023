491f21ea8f949a5caa5b02a0e63be0da67c26cc0
tinchopr <tinchop@gmail.com>
2023-09-23 19:17:06 -0300

    add excel link

--- ExemptRates.py
+++ ExemptRates.py
@@ -5,7 +5,7 @@ import requests
 import re
 from bs4 import BeautifulSoup
 from datetime import datetime
-# import xlwings as xw
+import xlwings as xw
 
 '''
 La idea de este programa es descargar una tasa específica de una pógina del 
@@ -39,11 +39,11 @@ rates for the current month and the prior two months.) 1.44%
 # Finalmente, devuelve el texto acumulado que es el contenido completo del archivo PDF.
 
 
+# This function remains the same
 def extract_text_from_pdf(url):
     response = requests.get(url)
     pdf_file_object = io.BytesIO(response.content)
-    # pdf_reader = PyPDF2.PdfFileReader(pdf_file_object)
-    pdf_reader = PyPDF4.PdfFileReader(pdf_file_object)  
+    pdf_reader = PyPDF4.PdfFileReader(pdf_file_object)
     count = pdf_reader.numPages
     text = ""
     for i in range(count):
@@ -51,102 +51,99 @@ def extract_text_from_pdf(url):
         text += page.extractText()
     return text
 
-'''
-Parte comentada dado que específicio para un excel del trabajo
-'''
-##############################
+# Get the active workbook and sheet
+wb = xw.books.active
+ws = wb.sheets['ExemptRates']
 
-# # Obtén el libro de trabajo activo y la hoja
-# wb = xw.books.active
-# ws = wb.sheets['ExemptRates']
+# Create a DataFrame from the active sheet's data
+exempt_rate_table = ws.range('A1').expand().options(pd.DataFrame, index=False, header=True).value
 
-# # Crea un DataFrame a partir de los datos de la hoja activa
-# exempt_rate_table = ws.range('A1').expand().options(pd.DataFrame, index=False, header=True).value
+# Create a new column for the address of month
+exempt_rate_table['address'] = ['A' + str(i) for i in range(2, len(exempt_rate_table) + 2)]
 
-# # Crea una nueva columna para la dirección del mes
-# exempt_rate_table['address'] = ['A' + str(i) for i in range(2, len(exempt_rate_table) + 2)]
+# Set 'month' as index
+exempt_rate_table.set_index('Month', inplace=True)
 
-# # Establece 'month' como índice
-# exempt_rate_table.set_index('Month', inplace=True)
-
-##############################
-
-'''
-Sigue el programa
-'''
-
-# Realiza una solicitud a la página principal del IRS
+# make a request to the IRS main page
 url = 'https://www.irs.gov/applicable-federal-rates'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-# Esta línea de código crea una "plantilla" que nos ayuda a encontrar partes específicas del texto. Se hace una sola vez para ahorrar tiempo.
+# I moved the regex pattern outside of the loop to avoid unnecessary recompilation
+# REGEX_TEXT_TO_FIND = re.compile(r"Long\s*-\s*term\s*\n*\s*\n*adjusted\s*AFR\s*([\s\S]{6})")  
 REGEX_TEXT_TO_FIND = re.compile(r"and\s*the\s*prior\s*two\s*months.\)\s*([\s\S]{10})")
 
-# Crea una lista para almacenar los datos extraídos
+
+# create list to store the extracted data
 data = []
 pdf_counter = 0
 
 
-# Aquí recorremos todos los elementos 'a' en la página
+# Here we go through all 'a' elements in the page
 for link in soup.find_all('a'):
 
-    # Verifica si href existe y cumple tus condiciones
+    # Check if href exists and meets your conditions
     href = link.get('href')
     if href and href.endswith('.pdf') and 'rr' in href:  
         pdf_path = "https://www.irs.gov" + href
         
-        # if pdf_path not in exempt_rate_table['Link'].values:
+        if pdf_path not in exempt_rate_table['Link'].values:
             
-        # Extrae el texto completo del enlace PDF
-        full_text = extract_text_from_pdf(pdf_path)
+            # Extract full text from the PDF link
+            full_text = extract_text_from_pdf(pdf_path)
+    
+            # Initialize dictionary to store data for each link
+            link_data = {'link': pdf_path}
+    
+            # Extract rate using regex
+            match = REGEX_TEXT_TO_FIND.search(full_text)
+            link_data['rate'] = match.group(1) if match else None
+    
+            # Remove line breaks from the rate if it exists 
+            if link_data.get('rate'):
+                link_data['rate'] = link_data['rate'].replace('\n', '')
+            
+            # Remove characters that are not numbers or periods
+            link_data['rate'] = re.sub(r'[^0-9.]', '', link_data['rate']) if link_data.get('rate') else None
+    
+            # Extract the previous 30 characters before "(the current month)"
+            start_position = full_text.find("(the current month)")
+            if start_position != -1:
+                start = max(start_position - 30, 0)  # Using max to handle negative start index
+                previous_30_characters = full_text[start:start_position]
+                link_data['month'] = previous_30_characters
+            else:
+                link_data['month'] = None
+    
+            # Append link_data to data
+            data.append(link_data)
+        # else:
+        #     sys.exit()
 
-        # Inicializa el diccionario para almacenar los datos de cada enlace
-        link_data = {'link': pdf_path}
+# #Exit the code if not updated is needed
+# if pdf_counter == 0:
+#     sys.exit()
 
-        # Extrae la tasa usando regex
-        match = REGEX_TEXT_TO_FIND.search(full_text)
-        link_data['rate'] = match.group(1) if match else None
-
-        # Elimina los saltos de línea de la tasa si existe
-        if link_data.get('rate'):
-            link_data['rate'] = link_data['rate'].replace('\n', '')
-        
-        # Elimina los caracteres que no son números o puntos
-        link_data['rate'] = re.sub(r'[^0-9.]', '', link_data['rate']) if link_data.get('rate') else None
-
-        # Extrae los 30 caracteres anteriores a "(el mes actual)"
-        start_position = full_text.find("(the current month)")
-        if start_position != -1:
-            start = max(start_position - 30, 0)  # Utilizando max para manejar un índice de inicio negativo
-            previous_30_characters = full_text[start:start_position]
-            link_data['month'] = previous_30_characters
-        else:
-            link_data['month'] = None
-
-        # Agrega link_data a los datos
-        data.append(link_data)
-
-#Convertir a número
-# Define una función para limpiar los valores y convertirlos en porcentajes
+#Convert to number
+# Define a function to clean the values and convert them to percentages
 def convert_to_number(x):
-    x = x.replace('\n','')  # Esto elimina los saltos de línea
-    x = x.replace('%','')  # Esto elimina los saltos de línea
-    x = re.sub(r'(\d) (\d)', r'\1\2', x)  # Esto elimina cualquier espacio entre dos números
+    x = x.replace('\n','')  # This removes line breaks
+    x = x.replace('%','')  # This removes line breaks
+    x = re.sub(r'(\d) (\d)', r'\1\2', x)  # This removes any space between two numbers
     x = float(x)
     return x
 
-# Convierte la lista de diccionarios en un DataFrame
+# Convert the list of dictionaries to a DataFrame
 df = pd.DataFrame(data)
 df['rate'] = df['rate'].apply(convert_to_number)
 
-# Define una función para limpiar los valores y convertirlos en porcentajes
+# Define a function to clean the values and convert them to percentages
 def clean_text(x):
-    x = x.replace('\n','')  # Esto elimina los saltos de línea
-    x = re.sub(r'(\d) (\d)', r'\1\2', x)  # Esto elimina cualquier espacio entre dos números
+    x = x.replace('\n','')  # This removes line breaks
+    x = re.sub(r'(\d) (\d)', r'\1\2', x)  # This removes any space between two numbers
     return x
 
-#Limpia Mes del DF
+#Clean Month DF
 df['month'] = df['month'].apply(clean_text)
 
 def extract_date(x):
@@ -161,37 +158,29 @@ def convert_to_date_format(x):
     except ValueError:
         return ''
 
-#Convierte Mes a formato de fecha
+#Convert Month to date format
 df['month'] = df['month'].apply(convert_to_date_format)
 df['month'] = pd.to_datetime(df['month'], format='%m/%Y')
 
-# Establece 'month' como índice
+# Set 'month' as index
 df.set_index('month', inplace=True)
 
-#Ordena DataFrame por índice 'month'
+# Sorting DataFrame by 'month' index
 df.sort_index(inplace=True)
 
-# Reorganiza las columnas
+# Rearrange the columns
 df = df[['rate','link']]
 
-#Transforma las tasas a %
+#Transform rates to %
 df['rate'] = df['rate']/100
 
-# Imprime resultados en la consola
-print(df)
+# #Merge Exempt Rata DF and 
+final_df = exempt_rate_table.merge(df, left_index=True, right_index=True)
+final_df = final_df.rename(columns={'link': 'IRS Link'})
+final_df = final_df[final_df['Link'].isnull()]
+final_df = final_df.drop(['Link',final_df.columns[0]],axis=1)
 
-'''
-Parte comentada dado que específicio para un excel del trabajo
-'''
-##############################
-# # Fusiona el DataFrame de Tasa Exenta y 
-# final_df = exempt_rate_table.merge(df, left_index=True, right_index=True)
-# final_df = final_df.rename(columns={'link': 'IRS Link'})
-# final_df = final_df[final_df['Link'].isnull()]
-# final_df = final_df.drop(['Link',final_df.columns[0]],axis=1)
-
-# for index, row in final_df.iterrows():
-#     ws.range(row['address']).offset(0,1).value = row['rate']
-#     ws.range(row['address']).offset(0,2).value = row['IRS Link']
-
-##############################
+for index, row in final_df.iterrows():
+    # print(row['address'], row['rate'], row['IRS Link'])
+    ws.range(row['address']).offset(0,1).value = row['rate']
+    ws.range(row['address']).offset(0,2).value = row['IRS Link']
