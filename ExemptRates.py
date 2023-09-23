05ac56bc91438d8398b375b650fe6e0217573d60
tinchopr <tinchop@gmail.com>
2023-09-23 19:59:23 -0300

    fix to work with a single excel

--- ExemptRates.py
+++ ExemptRates.py
@@ -66,7 +66,13 @@ exempt_rate_table.set_index('Month', inplace=True)
 
 # make a request to the IRS main page
 url = 'https://www.irs.gov/applicable-federal-rates'
-response = requests.get(url)
+
+headers = {
+    'User-Agent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+}
+
+response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.text, 'html.parser')
 
 # I moved the regex pattern outside of the loop to avoid unnecessary recompilation
